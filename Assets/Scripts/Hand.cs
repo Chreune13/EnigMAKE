26b35e0a4c65ea0e92079d4e29525b8c8eb6bb7b
celine <ctran@et.esiea.fr>
2022-11-02 17:13:02 +0100

    hand animation

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    Animator animator;
+    private float triggerTarget;
+
+    private float triggerCurrent;
+
+    public float speed;
+
+    private string animatorTriggerParam = "Trigger";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+}
