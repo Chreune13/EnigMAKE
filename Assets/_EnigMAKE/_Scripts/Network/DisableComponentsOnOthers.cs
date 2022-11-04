eddcec00c7fa313130e13164acbd5d57c2bb3744
Engel Adrien <72760092+adriengel974@users.noreply.github.com>
2022-11-04 15:15:32 +0100

    Network first implementation

---
+++ Assets/_EnigMAKE/_Scripts/Network/DisableComponentsOnOthers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableComponentsOnOthers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Component[] components = GetComponents(typeof(MonoBehaviour));
+
+        //foreach(Component component in components)
+        //{
+        //    Component obj = component as Transform;
+        //    if (obj != null)
+        //        continue;
+
+        //    obj.
+            
+        //}
+    }
+}
