3f44eb06b038e40b5c82ebcf46dab39c6f9a23e7
celine-git <70598482+Luisleb@users.noreply.github.com>
2022-11-04 17:25:09 +0100

    Sync update

--- Assets/_EnigMAKE/_Scripts/Network/DisableComponentsOnOthers.cs
+++ Assets/_EnigMAKE/_Scripts/Network/DisableComponentsOnOthers.cs
@@ -6,10 +6,9 @@ using UnityEngine;
 
 public class DisableComponentsOnOthers : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        if(!IsOwner)
+        if (!IsOwner)
         {
             MonoBehaviour[] components = GetComponents<MonoBehaviour>();
 
