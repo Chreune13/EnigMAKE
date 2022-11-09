562efb264e905c447784260486aa1350c6546155
Cheune13 <80524613+Chreune13@users.noreply.github.com>
2022-11-09 16:29:43 +0100

    chest anim + prefab

---
+++ Assets/_EnigMAKE/_Scripts/Interactions/Chest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Enigme
+{
+    override protected void ExecuteAction1()
+    {
+
+        GetComponent<Animator>().SetBool("ChestOpen", true);
+
+
+    }
+}
