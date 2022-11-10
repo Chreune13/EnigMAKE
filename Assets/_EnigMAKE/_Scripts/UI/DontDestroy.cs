f70b3de2e30c52594211c70ea8b897252905440b
Engel Adrien <72760092+adriengel974@users.noreply.github.com>
2022-11-10 11:17:17 +0100

    update network

--- Assets/_EnigMAKE/_Scripts/UI/DontDestroy.cs
+++ Assets/_EnigMAKE/_Scripts/UI/DontDestroy.cs
@@ -6,22 +6,7 @@ public class DontDestroy : MonoBehaviour
 {
     void Awake()
     {
-
-        //Debug.Log("DontDestroy script disabled");
-        GameObject[] player = GameObject.FindGameObjectsWithTag("ClientOnly");
-
-        if (this.gameObject.tag == "ClientOnly")
-        {
-            if (player.Length > 1)
-            {
-                Destroy(this.gameObject);
-            }
-
-            DontDestroyOnLoad(this.gameObject);
-
-        }
-
-            DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 }
 
