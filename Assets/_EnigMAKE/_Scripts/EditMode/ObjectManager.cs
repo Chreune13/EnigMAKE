3e2fb5700c520e74526ccfafac198ff11a1bbddf
Engel Adrien <72760092+adriengel974@users.noreply.github.com>
2022-12-12 10:57:34 +0100

    Construction object raycast test

--- Assets/_EnigMAKE/_Scripts/EditMode/ObjectManager.cs
+++ Assets/_EnigMAKE/_Scripts/EditMode/ObjectManager.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(XRGrabInteractable))]
-public class ObjectScale : MonoBehaviour
+public class ObjectManager : MonoBehaviour
 {
+    //------------------------ Object raycast ----------------------
+
+
+    private void FixedUpdate()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (!isGrabbed)
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 100.0f))
+            {
+                //print("Found an object - distance: " + hit.distance);
+                //print("Found an object - normal: " + hit.normal);
+                Debug.DrawRay(ray.origin, ray.direction, Color.blue);
+
+                transform.localRotation = hit.transform.localRotation;
+            }
+        }
+    }
+
+    //--------------------------------------------------------------
+
+    //-------------------- Object Scale on Grab --------------------
+
     private Vector3 doorScaleFactor = new Vector3(0.8f,0.8f,0.8f);
     private Vector3 keyScaleFactor = new Vector3(0.5f, 0.5f, 0.5f);
 
     XRGrabInteractable grabInteractable;
+    private bool isGrabbed = false;
 
     private void Awake()
     {
@@ -28,14 +53,16 @@ public class ObjectScale : MonoBehaviour
 
     public void ObjectIsGrabbed(SelectEnterEventArgs args)
     {
+        isGrabbed = true;
+
         if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
         {
-
+            //
         }
 
         if (PlayerState.Singleton.playerState == PlayerType.PLAYER)
         {
-
+            //
         }
 
         if (PlayerState.Singleton.playerState == PlayerType.EDIT)
@@ -47,16 +74,18 @@ public class ObjectScale : MonoBehaviour
         }
     }
 
-    void ObjectIsNotGrabbed(SelectExitEventArgs args)
+    public void ObjectIsNotGrabbed(SelectExitEventArgs args)
     {
+        isGrabbed = false;
+
         if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
         {
-
+            //
         }
 
         if (PlayerState.Singleton.playerState == PlayerType.PLAYER)
         {
-
+            //
         }
 
         if (PlayerState.Singleton.playerState == PlayerType.EDIT)
@@ -67,4 +96,6 @@ public class ObjectScale : MonoBehaviour
                 transform.localScale -= keyScaleFactor;
         }
     }
+
+    //-------------------------------------------------------------
 }
