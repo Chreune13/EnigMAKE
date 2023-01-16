c239c8ebad442d2e98a28b36a70cd93e9e9ab830
celine <ctran@et.esiea.fr>
2023-01-16 18:12:39 +0100

    modif UI front shape / grab medieval /rename scene / audio click UI

--- Assets/_EnigMAKE/_Scripts/Tutorial/Deplacer3Tuto.cs
+++ Assets/_EnigMAKE/_Scripts/Tutorial/Deplacer3Tuto.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +11,8 @@ public class Deplacer3Tuto : MonoBehaviour
     [SerializeField] GameObject DEPLACERGroup;
     [SerializeField] GameObject Table;
     [SerializeField] GameObject UI;
+    [SerializeField] GameObject Affiche3;
+   // [SerializeField] GameObject[] gameobjects;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Hand")
@@ -18,7 +21,6 @@ public class Deplacer3Tuto : MonoBehaviour
             StartCoroutine(Wait(other.gameObject));
         }
     }
-
     IEnumerator Wait(GameObject other)
     {
         yield return new WaitForSeconds(1f);
@@ -27,6 +29,8 @@ public class Deplacer3Tuto : MonoBehaviour
         TeleportationArea.SetActive(true);
         Table.SetActive(true);
         UI.SetActive(true);
+
+        Affiche3.SetActive(false);
         DEPLACERGroup.SetActive(false);
 
     }
