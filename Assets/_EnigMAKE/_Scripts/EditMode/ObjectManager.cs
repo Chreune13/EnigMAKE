399f7d5fea2c7374a08f4f6adb7c6d8946c53627
Engel Adrien <72760092+adriengel974@users.noreply.github.com>
2023-01-17 16:40:44 +0100

    Revert "objectManager updated"
    This reverts commit c5cec97aef97754ac5b283c7f4b5d4dcfba54c89.

--- Assets/_EnigMAKE/_Scripts/EditMode/ObjectManager.cs
+++ Assets/_EnigMAKE/_Scripts/EditMode/ObjectManager.cs
@@ -6,22 +6,29 @@ using UnityEngine.XR.Interaction.Toolkit;
 [RequireComponent(typeof(XRGrabInteractable))]
 public class ObjectManager : MonoBehaviour
 {
+    new Collider collider;
+    new Renderer renderer;
 
-
-    [SerializeField]
-    private float stickDistance = 0.3f;
-
-    private Quaternion initialRotation;
-
-    [SerializeField]
-    private float gridSize = 0.5f;
-
-    XRGrabInteractable grabInteractable;
-    private bool isGrabbed = false;
-
-    private void Awake()
+    public void Start()
     {
-<<<<<<< Updated upstream
+        collider = GetComponent<Collider>();
+        renderer = GetComponent<Renderer>();
+    }
+
+
+    //------------------------ Object raycast ----------------------
+
+
+    private void FixedUpdate()
+    {   
+        if ((int)PlayerState.Singleton.playerState == 2)
+        {
+            if(!isGrabbed) stickToSurface();
+        }
+    }
+
+    private void stickToSurface()
+    {
         //Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit hit;
 
@@ -46,34 +53,11 @@ public class ObjectManager : MonoBehaviour
             //    parentTransform.position = new Vector3(parentTransform.position.x, YPos, parentTransform.position.z);
             //}
         }
-=======
-        grabInteractable = GetComponent<XRGrabInteractable>();
 
-        grabInteractable.selectEntered.AddListener(ObjectIsGrabbed);
-        grabInteractable.selectExited.AddListener(ObjectIsNotGrabbed);
-    }
-
-    private void OnDisable()
-    {
-        grabInteractable.selectEntered.RemoveAllListeners();
-        grabInteractable.selectExited.RemoveAllListeners();
-    }
-
-    private void FixedUpdate()
-    {
-        initialRotation = transform.rotation;
-        stickToSurface();
-    }
->>>>>>> Stashed changes
-
-    private void stickToSurface()
-    {
-        RaycastHit hit;
-        if (!isGrabbed)
+        if(renderer != null && collider == null)
         {
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, stickDistance))
+            if (Physics.Raycast(transform.position - new Vector3(0, renderer.bounds.size.y / 2, 0), Vector3.down, out hit, 0.3f))
             {
-<<<<<<< Updated upstream
                 //print("Found an object - distance: " + hit.distance);
                 //print("Found an object - normal: " + hit.normal);
                 //Debug.DrawRay(ray.origin, ray.direction, Color.blue);
@@ -86,50 +70,32 @@ public class ObjectManager : MonoBehaviour
                 transform.position = new Vector3(transform.position.x, YPos, transform.position.z);
                 transform.rotation = tf.rotation;
 
-=======
-                // Keep the last rotation
-                transform.rotation = initialRotation;
-
-                // Change the position of the object so it "sticks" to a surface 
-                transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
-                transform.position = new Vector3(Mathf.Round(transform.position.x / gridSize) * gridSize,
-                                                Mathf.Round(transform.position.y / gridSize) * gridSize,
-                                                Mathf.Round(transform.position.z / gridSize) * gridSize);
->>>>>>> Stashed changes
             }
         }
-    }
 
-    /*private void stickToSurface()
-    {
-        if (!isGrabbed)
+        if(renderer == null && collider != null)
         {
-            float sphereRadius = 0.2f;
-            float stickDistance = 0.3f;
-            Vector3 sphereCastOrigin = transform.position;
-            Quaternion sphereCastRotation = Quaternion.identity;
-            Vector3 sphereCastDirection = Vector3.down;
-
-            RaycastHit hit;
-            if (Physics.SphereCast(sphereCastOrigin, sphereRadius, sphereCastDirection, out hit, stickDistance))
+            if (Physics.Raycast(transform.position - new Vector3(0, collider.bounds.size.y / 2, 0), Vector3.down, out hit, 0.3f))
             {
-                // Keep the last rotation
-                transform.rotation = initialRotation;
+                //print("Found an object - distance: " + hit.distance);
+                //print("Found an object - normal: " + hit.normal);
+                //Debug.DrawRay(ray.origin, ray.direction, Color.blue);
 
-<<<<<<< Updated upstream
+                // Change orientation of the objet
+                transform.up = hit.transform.up;
+
                 //Change the position of the object so it "sticks" to a surface 
                 float YPos = hit.transform.position.y + collider.bounds.size.y / 2 + hit.collider.bounds.size.y;
                 transform.position = new Vector3(transform.position.x, YPos, transform.position.z);
                 transform.rotation = tf.rotation;
 
-=======
-                // Change the position of the object so it "sticks" to a surface 
-                transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
->>>>>>> Stashed changes
             }
-            else
+        }
+
+        if(renderer != null && collider != null)
+        {
+            if (Physics.Raycast(transform.position /*- new Vector3(0, collider.bounds.size.y / 2, 0)*/, Vector3.down, out hit, 0.3f))
             {
-<<<<<<< Updated upstream
                 //print("Found an object - distance: " + hit.distance);
                 //print("Found an object - normal: " + hit.normal);
                 //Debug.DrawRay(ray.origin, ray.direction, Color.blue);
@@ -192,27 +158,6 @@ public class ObjectManager : MonoBehaviour
         Debug.Log("Default");
         return Vector3.down;
     }*/
-=======
-                sphereCastDirection = Vector3.up;
-                if (Physics.SphereCast(sphereCastOrigin, sphereRadius, sphereCastDirection, out hit, stickDistance))
-                {
-                    // Keep the last rotation
-                    transform.rotation = initialRotation;
-
-                    // Change the position of the object so it "sticks" to a surface 
-                    transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
-                }
-                else if()
-                {
-                    sphereCastDirection = Vector3.left;
-                    if (Physics.SphereCast(sphereCastOrigin, sphereRadius, sphereCastDirection, out hit, stickDistance))
-                    {
-                        // Keep the last rotation
-                        transform.rotation = initialRotation;
-
-                        // Change the position of the object so it "sticks" to a surface 
-                        transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z)*/
->>>>>>> Stashed changes
 
     //--------------------------------------------------------------
 
@@ -224,11 +169,28 @@ public class ObjectManager : MonoBehaviour
     private Vector3 doorScaleFactor = new Vector3(0.8f,0.8f,0.8f);
     private Vector3 keyScaleFactor = new Vector3(0.5f, 0.5f, 0.5f);
 
+    XRGrabInteractable grabInteractable;
+    private bool isGrabbed = false;
+
+    private void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        grabInteractable.selectEntered.AddListener(ObjectIsGrabbed);
+        grabInteractable.selectExited.AddListener(ObjectIsNotGrabbed);
+    }
+
+    private void OnDisable()
+    {
+        grabInteractable.selectEntered.RemoveAllListeners();
+        grabInteractable.selectExited.RemoveAllListeners();
+    }
+
     public void ObjectIsGrabbed(SelectEnterEventArgs args)
     {
         isGrabbed = true;
 
-        /*if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
+        if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
         {
             //
         }
@@ -236,7 +198,7 @@ public class ObjectManager : MonoBehaviour
         if (PlayerState.Singleton.playerState == PlayerType.PLAYER)
         {
             //
-        }*/
+        }
 
         if (PlayerState.Singleton.playerState == PlayerType.EDIT)
         {
@@ -251,7 +213,7 @@ public class ObjectManager : MonoBehaviour
     {
         isGrabbed = false;
 
-        /*if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
+        if (PlayerState.Singleton.playerState == PlayerType.GAMEMASTER)
         {
             //
         }
@@ -259,7 +221,7 @@ public class ObjectManager : MonoBehaviour
         if (PlayerState.Singleton.playerState == PlayerType.PLAYER)
         {
             //
-        }*/
+        }
 
         if (PlayerState.Singleton.playerState == PlayerType.EDIT)
         {
