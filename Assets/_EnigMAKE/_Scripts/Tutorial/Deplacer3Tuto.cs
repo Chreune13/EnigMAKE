9d4120cbe2eba498945741acf5f68d25ffbee77b
celine <ctran@et.esiea.fr>
2023-02-01 10:58:38 +0100

    Tutorial Sound validation

--- Assets/_EnigMAKE/_Scripts/Tutorial/Deplacer3Tuto.cs
+++ Assets/_EnigMAKE/_Scripts/Tutorial/Deplacer3Tuto.cs
@@ -6,12 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Deplacer3Tuto : MonoBehaviour
 {
-    [SerializeField] GameObject FinPainting;
-    [SerializeField] GameObject TeleportationArea;
-    [SerializeField] GameObject DEPLACERGroup;
-    [SerializeField] GameObject Table;
-    [SerializeField] GameObject UI;
-    [SerializeField] GameObject Affiche3;
+    //[SerializeField] GameObject FinPainting;
+    //[SerializeField] GameObject TeleportationArea;
+    //[SerializeField] GameObject DEPLACERGroup;
+    //[SerializeField] GameObject Table;
+    //[SerializeField] GameObject UI;
+    //[SerializeField] GameObject Affiche3;
+
+    [SerializeField] GameObject[] Display;
+    [SerializeField] GameObject[] Hide;
+
+    [SerializeField] AudioSource validateSound;
+
    // [SerializeField] GameObject[] gameobjects;
     private void OnTriggerEnter(Collider other)
     {
@@ -20,18 +26,29 @@ public class Deplacer3Tuto : MonoBehaviour
 
             StartCoroutine(Wait(other.gameObject));
         }
+        validateSound.Play();
     }
     IEnumerator Wait(GameObject other)
     {
         yield return new WaitForSeconds(1f);
 
-        FinPainting.SetActive(true);
-        TeleportationArea.SetActive(true);
-        Table.SetActive(true);
-        UI.SetActive(true);
+        //FinPainting.SetActive(true);
+        //TeleportationArea.SetActive(true);
+        //Table.SetActive(true);
+        //UI.SetActive(true);
 
-        Affiche3.SetActive(false);
-        DEPLACERGroup.SetActive(false);
+        //Affiche3.SetActive(false);
+        //DEPLACERGroup.SetActive(false);
+
+        foreach (var item in Display)
+        {
+            item.SetActive(true);
+        }
+
+        foreach (var item in Hide)
+        {
+            item.SetActive(false);
+        }
 
     }
 }
